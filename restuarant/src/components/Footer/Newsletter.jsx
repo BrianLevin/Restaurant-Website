c2fd71e84abd165556e09987458a325a62c8c65b
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-04-20 11:46:40 -0400

    Footer Newsletter Sections and npm build project

--- restuarant/src/components/Footer/Newsletter.jsx
+++ restuarant/src/components/Footer/Newsletter.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className = "app_newsletter">
+    <div className="app_newsletter-heading">
+      <SubHeading title= "Newsletter" />
+      <h1 className= "headtext__cormorant">Subscribe</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>
+    </div>
+    <div className="app_newsletter-input flex__center" >
+<input type= "email"  placeholder= "Enter your email address"/>
+<button className= "custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
